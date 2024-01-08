b361cd799e3da7586dcf084d76a7f7fa47ca0db1
Juan Fernando <juanfernandodemourasilva@gmail.com>
2024-01-08 18:01:14 -0300

    aula 37

--- aula37/index.js
+++ aula37/index.js
@@ -3,6 +3,8 @@ const pessoa = {
   sobrenome: 'Fernando'
 };
 
+const nomes=["Juan", "pedro", "luiz"];
+
 // For clássico - Geralmente com iteráveis (array ou strings)
 // For in - Retorna o índice ou chave (string, array ou objetos)
 // For of - Retorna o valor em si (iteráveis, arrays ou strings)
