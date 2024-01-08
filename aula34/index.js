3f43ba5ba5d7a26bf14669b64afb391b09a6780f
Juan Fernando <juanfernandodemourasilva@gmail.com>
2024-01-08 12:12:44 -0300

    aula 34

--- aula34/index.js
+++ aula34/index.js
@@ -1,3 +1,7 @@
 const frutas = ['Maçã', 'Pêra', 'Uva'];
 const novasFrutas = frutas.map(el => el.toLowerCase());
 console.log(novasFrutas); // [ 'maçã', 'pêra', 'uva' ]
+
+for (let i = 0; i < 9; i++) {
+  console.log(i);
+}
